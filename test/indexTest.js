92a4a1ce28b6a6277d4aabf941cfc927de2aa978
Aaron Bruckner <aaronbruckner@gmail.com>
2016-12-12 20:06:54 -0800

    Hello world test framework added.

---
+++ test/indexTest.js
@@ -0,0 +1,9 @@
+'use strict';
+var sinon = require('sinon');
+var assert = require('chai').assert;
+
+describe('index.js', function () {
+    it('should say hello world', function () {
+        assert.isOk(true);
+    });
+});
