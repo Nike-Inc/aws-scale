1b2285fdc8e89b2c55f4ab67bfd5ef29550e7b91
Aaron Bruckner <aaronbruckner@gmail.com>
2016-12-16 16:41:50 -0800

    ResourceSet now scales and waits for all resources to respond.

--- src/ResourceSet.js
+++ src/ResourceSet.js
@@ -22,12 +22,26 @@ ResourceSet.prototype.getResources = function () {
   return this.resources;
 };
 
-/**
- *
- * @private
- */
-function _scale() {
+ResourceSet.prototype.scale = function (callback) {
+  var self = this;
+  callback = callback || function () {};
+  if (self.resources.length === 0) {
+    callback(null, []);
+  }
 
-}
+  var results = [];
+  var success = true;
+  for (var i = 0; i < self.resources.length; i++) {
+    self.resources[i].scale(function (result) {
+      results.push(result);
+      success = success && (result.status === 'success');
+
+      // All results finished, invoke callback
+      if (results.length >= self.resources.length) {
+        success ? callback(null, results) : callback(results);
+      }
+    });
+  }
+};
 
 module.exports = ResourceSet;
--- test/src/ResourceSetTest.js
+++ test/src/ResourceSetTest.js
@@ -68,20 +68,116 @@ describe('ResourceSet', function () {
 
   describe('Scale', function () {
 
+    var callback;
+    var resource1;
+    var resource2;
+
     beforeEach(function () {
+      callback = sinon.spy();
 
+      resource1 = new DynamoDB({TableName: 'table1'});
+      sinon.stub(resource1, 'scale');
+      resource2 = new DynamoDB({TableName: 'table2'});
+      sinon.stub(resource2, 'scale');
     });
 
-    describe('DynamoDB', function () {
+    it ('should return empty array if set contains no resources', function () {
+      resourceSet.scale(callback);
 
-      it('should call AWS SDK for DynamoDB', function () {
-
-      });
-
-      it('should call AWS SDK with proper params', function () {
-
-      });
-
+      assert.isTrue(callback.calledOnce, 'should invoke callback immediately if there are no resources to scale');
+      assert.isTrue(callback.calledWith(null, []), 'should return empty results object');
     });
+
+    it ('should not fail if no callback was provided and set contains no resources', function () {
+      resourceSet.scale();
+    });
+
+    it ('should call scale on each resource', function () {
+      resourceSet.add(resource1);
+      resourceSet.add(resource2);
+
+      resourceSet.scale(callback);
+
+      assert.isTrue(resource1.scale.calledOnce, 'should scale resource1');
+      assert.isTrue(resource1.scale.calledWith(sinon.match.func), 'should provide callback function');
+      assert.isTrue(resource2.scale.calledOnce, 'should scale resource2');
+      assert.isTrue(resource2.scale.calledWith(sinon.match.func), 'should provide callback function')
+    });
+
+    it('should return array of results with no error if all are successful', function () {
+      resourceSet.add(resource1);
+      resourceSet.add(resource2);
+
+      resourceSet.scale(callback);
+
+      assert.isFalse(callback.called, 'callback should not be invoked until all resources respond');
+
+      // Resource 1 finishes
+      var resource1Result = {
+        type: 'DynamoDB',
+        name: 'table1',
+        status: 'success'
+      };
+      resource1.scale.callArgWith(0, resource1Result);
+
+      assert.isFalse(callback.called, 'callback should not be invoked until all resources respond');
+
+      // Resource 2 finishes
+      var resource2Result = {
+        type: 'DynamoDB',
+        name: 'table2',
+        status: 'success'
+      };
+      resource2.scale.callArgWith(0, resource2Result);
+
+      assert.isTrue(callback.calledOnce, 'should invoke callback after all resources return result');
+      assert.isTrue(callback.calledWith(null, [resource1Result, resource2Result]), 'should return results in an array');
+    });
+
+    it('should return array of results as an error if any resources failed', function () {
+      resourceSet.add(resource1);
+      resourceSet.add(resource2);
+
+      resourceSet.scale(callback);
+
+      assert.isFalse(callback.called, 'callback should not be invoked until all resources respond');
+
+      // Resource 1 finishes with failure
+      var resource1Result = {
+        type: 'DynamoDB',
+        name: 'table1',
+        status: 'failure',
+        error: 'somethingBadHappened'
+      };
+      resource1.scale.callArgWith(0, resource1Result);
+
+      assert.isFalse(callback.called, 'callback should not be invoked until all resources respond');
+
+      // Resource 2 finishes
+      var resource2Result = {
+        type: 'DynamoDB',
+        name: 'table2',
+        status: 'success'
+      };
+      resource2.scale.callArgWith(0, resource2Result);
+
+      assert.isTrue(callback.calledOnce, 'should invoke callback after all resources return result');
+      assert.isTrue(callback.calledWith([resource1Result, resource2Result]), 'should return results in an array as error');
+    });
+
+    it('should not fail if no callback was provided and all results are returned', function () {
+      resourceSet.add(resource1);
+
+      resourceSet.scale();
+
+      // Resource 1 finishes
+      var resource1Result = {
+        type: 'DynamoDB',
+        name: 'table1',
+        status: 'success'
+      };
+      resource1.scale.callArgWith(0, resource1Result);
+    });
+
   });
 });
