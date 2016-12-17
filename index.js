6039836bd3aae3dcc5fc049bec9e0c97115ae0fa
Aaron Bruckner <aaronbruckner@gmail.com>
2016-12-17 00:01:55 -0800

    Updated index.js to export aws-scale NPM module.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /**
- * Created by abruck on 12/12/16.
+ * Aws-scale module. Maps all modules to a single, importable one for NPM module.
  */
 
-exports.helloWorld = function () {
-  console.log("hello world");
-};
+exports.ResourceSet = require('./src/ResourceSet');
+
+// Resources
+exports.DynamoDB = require('./src/resources/DynamoDB');
+exports.AutoScaleGroup = require('./src/resources/AutoScaleGroup');
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,9 +1,24 @@
 'use strict';
 var sinon = require('sinon');
+var awsScale = require('../index');
 var assert = require('chai').assert;
 
+var ResourceSet = require('../src/ResourceSet');
+var DynamoDB = require('../src/resources/DynamoDB');
+var AutoScaleGroup = require('../src/resources/AutoScaleGroup');
+
 describe('index.js', function () {
-  it('should say hello world', function () {
-    assert.isOk(true);
+
+  it('should include ResourceSet', function () {
+    assert.isTrue(awsScale.ResourceSet === ResourceSet);
   });
+
+  it('should include DynamoDB', function () {
+    assert.isTrue(awsScale.DynamoDB === DynamoDB);
+  });
+
+  it('should include AutoScaleGroup', function () {
+    assert.isTrue(awsScale.AutoScaleGroup === AutoScaleGroup);
+  });
+
 });
